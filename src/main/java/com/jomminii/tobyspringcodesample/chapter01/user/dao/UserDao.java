5c0840e7faffc8ef0b32279db721449187f0b90b
jomminii <jomminii.dev@gmail.com>
2022-09-12 23:30:44 +0900

    [add] 1.1 초난감 DAO

--- src/main/java/com/jomminii/tobyspringcodesample/chapter01/user/dao/UserDao.java
+++ src/main/java/com/jomminii/tobyspringcodesample/chapter01/user/dao/UserDao.java
@@ -1,5 +1,62 @@
 package com.jomminii.tobyspringcodesample.chapter01.user.dao;
 
-public class UserDao {
+import com.jomminii.tobyspringcodesample.chapter01.user.domain.User;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/test",
+            "test",
+            "password"
+        );
+
+        PreparedStatement ps = c.prepareStatement(
+            "insert into users(id, name, password) values(?,?,?)"
+        );
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+
+    }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/test",
+            "test",
+            "password"
+        );
+
+        PreparedStatement ps = c.prepareStatement(
+            "select * from users where id = ?"
+        );
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
 }
--- src/main/java/com/jomminii/tobyspringcodesample/chapter01/user/domain/User.java
+++ src/main/java/com/jomminii/tobyspringcodesample/chapter01/user/domain/User.java
@@ -28,6 +28,4 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
-
 }
